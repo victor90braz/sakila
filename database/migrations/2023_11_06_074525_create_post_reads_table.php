7daec36cc337b6b918db7cdb520de9289c69688d
victor braz <victor.90braz@gmail.com>
2023-11-06 09:14:25 +0100

    post_reads

---
+++ database/migrations/2023_11_06_074525_create_post_reads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_reads', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id')->index();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->timestamps();
+
+            $table->foreign('post_id')
+            ->references('id')
+            ->on('posts');
+
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_reads');
+    }
+};
